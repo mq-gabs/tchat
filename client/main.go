e1566e07c303c3b697a0b3b590570b6206535d40
mq-gabs <marques.gabriel.dev@gmail.com>
2025-10-19 14:05:03 -0300

    refact: change input reader

--- client/main.go
+++ client/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
 	"tchat.com/client/command"
 )
 
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	var input string
 	var cont = true
 	var err error
 
 	for cont {
 		fmt.Printf("tchat > ")
-		fmt.Scanln(&input)
+
+		if !scanner.Scan() {
+			fmt.Println("cannot scan input")
+			return
+		}
+
+		input = scanner.Text()
 
 		cont, err = command.Exec(input)
 		if err != nil {
